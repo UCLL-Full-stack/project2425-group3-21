71d6c3ea3a663590ba733182d2fdd86e39017a09
VincentVanbrabantUCLL <r0900502@ucll.be>
2024-12-20 22:20:41 +0100

    Add strongness range validation message and update homepage test to mock translation

--- front-end/tests/homepage.test.tsx
+++ front-end/tests/homepage.test.tsx
@@ -3,10 +3,15 @@ import { render, screen } from '@testing-library/react';
 import HomeComponent from '../components/homeComponent';
 import '@testing-library/jest-dom';
 
-window.React = React;
+// Mock the useTranslation hook
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({
+    t: (key) => key,
+  }),
+}));
 
 test('renders component', () => {
-    render(<HomeComponent />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
-    });
+  render(<HomeComponent />);
+  const titleElement = screen.getByText('home.title');
+  expect(titleElement).toBeInTheDocument();
+});
